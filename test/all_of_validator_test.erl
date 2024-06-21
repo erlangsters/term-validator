7a6a7b1e7ff11da2131daf3e32ce77603ae0dff9
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2024-06-21 16:46:01 +0800

    Implement 'all of' validator

--- test/all_of_validator_test.erl
+++ test/all_of_validator_test.erl
@@ -11,4 +11,27 @@
 -include_lib("eunit/include/eunit.hrl").
 
 all_of_validator_test() ->
+    AllFormats = [
+        {number, [{min, 42}]},
+        {number, [{max, 42}]}
+    ],
+    Format = {all_of, AllFormats},
+
+    {invalid, {not_all_of, AllFormats}} = term_validator:validate(yolo, Format),
+
+    {invalid, {not_all_of, AllFormats}} = term_validator:validate(true, Format),
+    {invalid, {not_all_of, AllFormats}} = term_validator:validate(false, Format),
+    {invalid, {not_all_of, AllFormats}} = term_validator:validate(41, Format),
+    valid = term_validator:validate(42, Format),
+    {invalid, {not_all_of, AllFormats}} = term_validator:validate(42.5, Format),
+    {invalid, {not_all_of, AllFormats}} = term_validator:validate(43, Format),
+    {invalid, {not_all_of, AllFormats}} = term_validator:validate("Hello world!", Format),
+    {invalid, {not_all_of, AllFormats}} = term_validator:validate("Hi world!", Format),
+    {invalid, {not_all_of, AllFormats}} = term_validator:validate([], Format),
+    {invalid, {not_all_of, AllFormats}} = term_validator:validate({}, Format),
+    {invalid, {not_all_of, AllFormats}} = term_validator:validate(#{}, Format),
+
+    InvalidFormat = {all_of, [xyz, {abc, []}]},
+    {invalid_options, [xyz, abc]} = term_validator:validate(yolo, InvalidFormat),
+
     ok.
